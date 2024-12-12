6fe0050fbfe422cf1e95f12fe2bf2f643167491d
zahid47 <epiczahid@gmail.com>
2024-12-12 22:52:14 +0600

    feat:typesafe env

---
+++ env.mjs
@@ -0,0 +1,30 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  /*
+   * Serverside Environment variables, not available on the client.
+   * Will throw if you access these variables on the client.
+   */
+  server: {
+    TEST_KEY_SERVER: z.string().url(),
+  },
+  /*
+   * Environment variables available on the client (and server).
+   *
+   * 💡 You'll get type errors if these are not prefixed with NEXT_PUBLIC_.
+   */
+  client: {
+    TEST_KEY_CLIENT: z.string().min(1),
+  },
+  /*
+   * Due to how Next.js bundles environment variables on Edge and Client,
+   * we need to manually destructure them to make sure all are included in bundle.
+   *
+   * 💡 You'll get type errors if not all variables from `server` & `client` are included here.
+   */
+  runtimeEnv: {
+    TEST_KEY_SERVER: process.env.TEST_KEY_SERVER,
+    TEST_KEY_CLIENT: process.env.TEST_KEY_CLIENT,
+  },
+});
